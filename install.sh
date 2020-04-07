0f981eaff074aff5d526f0359474e18a0ca68128
REIS0 <aor@cesar.school>
2020-04-07 12:51:02 -0300

    Update install

--- install.sh
+++ install.sh
@@ -14,7 +14,6 @@ source $HOME/.cargo/env
 
 # install antibody
 curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-antibody bundle < .zsh_plugins.txt > .zsh_plugins.sh
 
 # install starship
 curl -fsSL https://starship.rs/install.sh | zsh
