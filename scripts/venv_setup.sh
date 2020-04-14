35e11167b3d45fc93942860b87c7a9fcc79a0f8f
REIS0 <aor@cesar.school>
2020-04-14 15:49:52 -0300

    Function

--- scripts/venv_setup.sh
+++ scripts/venv_setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # ___  ___  ___ ______ _____  ________   __
 # |  \/  | / _ \|  _  \  ___| | ___ \ \ / /
 # | .  . |/ /_\ \ | | | |__   | |_/ /\ V /
@@ -12,33 +12,21 @@
 # | |\ \| |___ _| |_/\__/ /\ |_/ /
 # \_| \_\____/ \___/\____/  \___/
 
-# Simple script for using virtualenv 
+# Simple script for using virtualenv
 # It installs virtualenv and creates a new
-# python 3 environment 
+# python 3 environment
 
 # APENAS RODAR, SO PRECISA TER O PIP FUNCIONANDO
+function venv_setup() {
+	# checa se o comando pra o virtualenv existe
+	if command -v virtualenv; then
+		continue
+	else
+		# instala o virtualenv e adiciona ao path (NAO FUNCIONA COM FISH)
+		pip install virtualenv
+	fi
 
-# checa se o comando pra o virtualenv existe
-if command -v virtualenv; then
-	continue
-else
-	# instala o virtualenv e adiciona ao path (NAO FUNCIONA COM FISH)
-	pip install virtualenv 
-	# if test -e $HOME/.zshrc; then
-	# 	echo export PATH=\"$HOME/.local/bin:$PATH\" >> $HOME/.zshrc
-	#	source $HOME/.zshrc
-	# elif test -e $HOME/.bashrc; then
-	#	echo export PATH=\"$HOME/.local/bin:$PATH\" >> $HOME/.bashrc
-	#	source $HOME/.bashrc
-	# else
-	#	echo "ERROR: nao pode colocar o path no arquivo"
-	# fi
-fi
-
-virtualenv -p python3 venv
-# se o python padrao for o 3 ja rodar o de baixo
-# virtualenv -p python venv
-
-. venv/bin/activate
-
-pip install -r req.txt
+	# se o python padrao for o 3 ja rodar o de baixo
+	# virtualenv -p python venv
+	virtualenv -p python3 venv
+}
