fe788a5b0ee81952ee53def58b4c9ebf1fb7e9f8
REIS0 <aor@cesar.school>
2020-06-29 13:01:12 -0300

    update installation packages

--- scripts/install.sh
+++ scripts/install.sh
@@ -6,7 +6,7 @@
 #chsh -s $(which zsh)
 
 # install fish, vim, fzf
-sudo apt install fish vim fzf trash-cli -y
+sudo apt install fish vim fzf trash-cli alacritty -y
 chsh -s $(which fish)
 
 # install rust
