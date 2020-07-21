04b82327f9dc5f1cff5567d2d71288113e43728a
REIS0 <aor@cesar.school>
2020-07-21 16:37:34 -0300

    add arch linux install script

---
+++ scripts/arch_install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# NEED GIT
+
+# must install packages
+sudo pacman -S xorg xorg-xinit xdg-user-dirs 
+
+# install yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+
+# install display manager
+yay -S lightdm
+sudo systemctl enable lightdm.service
+
+# install other stuff that I use
+yay -S i3 alacritty rofi xorg-xrandr dunst polybar-git thunar thunar-volman feh fish trash-cli
+
+# set login shell
+chsh -s $(which fish)
+
+# install rust
+curl https://sh.rustup.rs -sSf | sh
+source $HOME/.cargo/env
+
+# install aditional stuff using cargo
+cargo install lsd
+cargo install ripgrep
+
+echo "Reboot? [y/n]"
+
+read answer
+
+if [ $answer -eq "y"]; then
+	reboot
+else
+	echo "Reboot later for applying changes"
+fi
