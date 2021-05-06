088048008e46b8332bc4c03d5271ae1fa6b3001e
REIS0 <aor@cesar.school>
2021-05-05 21:46:31 -0300

    use exa's default ls colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,7 +8,7 @@ fundle plugin 'wfxr/forgit'
 
 fundle init
 
-alias ls='exa --icons --group-directories-first'
+alias ls='LS_COLORS=" " exa --icons --group-directories-first'
 alias l='ls -a'
 alias c='clear'
 #alias mv='mv -i'
