05f5fada73187a3fc086823435196c6ff733bb28
REIS0 <aor@cesar.school>
2021-05-17 09:42:10 -0300

    Update pop install

--- scripts/pop_install.sh
+++ scripts/pop_install.sh
@@ -1,36 +1,25 @@
-#!bin/bash
+#!/bin/bash
 
-# install zsh and vim
-# my main use is ubuntu based, just change for your distro
-#sudo apt install zsh vim -y
-#chsh -s $(which zsh)
+# My main use is ubuntu based, just change for your distro
 
 # install fish, vim, fzf
 sudo apt install fish vim fzf trash-cli alacritty -y
 chsh -s $(which fish)
 
-# install nvm (i hate this)
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# install fish plugins 
+fish -c "fundle install"
 
 # install rust
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
-# install antibody
-#curl -sfL git.io/antibody | sh -s - -b /usr/local/bin
-
-# install starship
-#curl -fsSL https://starship.rs/install.sh | zsh
-
 # install ls-deluxe
 cargo install bat || echo "bat installation failed"
 cargo install exa || echo "exa installation failed"
 cargo install ripgrep || echo "ripgrep installation failed"
+# install tealdeer dependency
+sudo apt install libssl-dev
 cargo install tealdeer || echo "tealdeer installation failed"
 
 echo "Restart terminal and it's done."
 echo "For Rust you need to logout and login again."
-
-# init fish
-fish
-fundle install
