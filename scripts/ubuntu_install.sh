efcadd0e189f559812e977f871a08c03b816f2d8
REIS0 <aor@cesar.school>
2021-09-23 20:41:21 -0300

    Update ubuntu installation script

--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,7 +3,7 @@
 # Ubuntu LTS
 
 # install fish, vim, fzf, etc
-sudo apt install fish vim fzf trash-cli curl gcc -y
+sudo apt install fish vim fzf trash-cli curl gcc bat ripgrep -y
 chsh -s $(which fish)
 
 # install fish plugins 
@@ -14,11 +14,12 @@ curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
 
 cargo install exa || echo "exa installation failed"
-cargo install bat || echo "bat installation failed"
-cargo install ripgrep || echo "ripgrep installation failed"
 # install tealdeer dependency
 sudo apt install libssl-dev
 cargo install tealdeer || echo "tealdeer installation failed"
+
+# Add cargo bin to path
+printf "\nif [ -d \"$HOME/.cargo/bin\" ] ; then\n\tPATH=\"$HOME/.cargo/bin:$PATH\"\nfi\n" >> $HOME/.profile
 
 echo "Restart terminal and it's done."
 echo "For Rust you need to logout and login again."
