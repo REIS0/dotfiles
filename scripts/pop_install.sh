42f3ebf4bef03bbfaabb139489835333e0d54e56
REIS0 <aoreis@protonmail.com>
2022-04-24 12:35:57 -0300

    Get ready for next LTS

--- scripts/pop_install.sh
+++ scripts/pop_install.sh
@@ -3,19 +3,11 @@
 # Pop_OS! 
 
 # install fish, vim, fzf
-sudo apt install fish vim fzf bat ripgrep exa trash-cli alacritty -y
+sudo apt install fish vim fzf bat ripgrep exa trash-cli tldr -y
 chsh -s $(which fish)
 
 # install fish plugins 
 fish -c "fundle install"
 
-# install rust
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-
-# install tealdeer dependency
-sudo apt install libssl-dev
-cargo install tealdeer || echo "tealdeer installation failed"
-
-echo "Restart terminal and it's done."
-echo "For Rust you need to logout and login again."
+# update tldr database
+tldr -u
--- scripts/ubuntu_install.sh
+++ scripts/ubuntu_install.sh
@@ -3,23 +3,11 @@
 # Ubuntu LTS
 
 # install fish, vim, fzf, etc
-sudo apt install fish vim fzf trash-cli curl gcc bat ripgrep -y
+sudo apt install fish vim fzf trash-cli curl gcc bat ripgrep tldr exa -y
 chsh -s $(which fish)
 
 # install fish plugins 
 fish -c "fundle install"
 
-# install rust
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-
-cargo install exa || echo "exa installation failed"
-# install tealdeer dependency
-sudo apt install libssl-dev
-cargo install tealdeer || echo "tealdeer installation failed"
-
-# Add cargo bin to path
-printf "\nif [ -d \"$HOME/.cargo/bin\" ] ; then\n\tPATH=\"$HOME/.cargo/bin:$PATH\"\nfi\n" >> $HOME/.profile
-
-echo "Restart terminal and it's done."
-echo "For Rust you need to logout and login again."
+# update tldr database
+tldr -u
