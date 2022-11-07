cd13b94396c437eab4186a4e2dada65e96f4d191
REIS0 <aoreis@protonmail.com>
2022-11-07 10:41:09 -0300

    Update suse script

--- scripts/tumbleweed_install.sh
+++ scripts/tumbleweed_install.sh
@@ -4,15 +4,11 @@
 #! NOT TESTED YET
 
 # install fish, vim, fzf
-sudo zypper install -y fish vim fzf tealdeer ripgrep exa bat alacritty alacritty-fish-completion
+sudo zypper install -y fish vim fzf tealdeer ripgrep exa bat fd
 chsh -s $(which fish)
 
 # install fish pluginsapt installapt install
 fish -c "fundle install"
 
-# install rust
-curl https://sh.rustup.rs -sSf | sh
-source $HOME/.cargo/env
-
-echo "Restart terminal and it's done."
-echo "For Rust you need to logout and login again."
+# create local fish file
+touch $HOME/.config/fish/local.fish
