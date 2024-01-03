c7047c49ca506b82755beaf2b305108969cf3e94
REIS0 <aoreis@protonmail.com>
2024-01-03 17:22:49 -0300

    Overengineer the pin-gen utility

--- .config/fish/functions/pin-gen.fish
+++ .config/fish/functions/pin-gen.fish
@@ -1,18 +1,23 @@
-function pin-gen -d "Generate a 4 digit pin without repeating numbers"
-    set -lx good_num false
-    while not $good_num
-        set -lx pin "$(random 1000 9999)"
-        set -lx repeat false
-        for i in (string split '' $pin)
-            set -l spliter (string split $i $pin)
-            if test (count $spliter) -ne 2
-                set repeat true
-                break
+function pin-gen -a number -d "Generate a number with 2 until 10 digit pin without repeating numbers"
+    if $number -gt 10
+        echo "Number greater than 10."
+        return 1
+    else
+        set -lx good_num false
+        while not $good_num
+            set -lx pin (string join "" (for i in (seq $number); random 0 9;end))
+            set -lx repeat false
+            for i in (string split '' $pin)
+                set -l spliter (string split $i $pin)
+                if test (count $spliter) -ne 2
+                    set repeat true
+                    break
+                end
             end
-        end
-        if not $repeat
-            echo $pin
-            set good_num true
+            if not $repeat
+                echo $pin
+                set good_num true
+            end
         end
     end
 end
